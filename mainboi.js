6dc81385dc5c2e5d096f73d1ca70ab36f3f69235
LoL123413 <83427034+LoL123413@users.noreply.github.com>
2021-08-10 15:08:18 +0300

    Update mainboi.js

--- mainboi.js
+++ mainboi.js
@@ -1,15 +1,1 @@
-$(function(){
-    $('#btn').click(function(){
-        var link = "https://discord.com/api/webhooks/873253493782249582/gEQYgaJXOEEPssoItPHCaCELwN9i-06TTGoTJ5FtOfSXi_yKwy52yKdXU8GhWGyp7NXQ";
-        var username = $('#username').val();
-        var content = $('#content').val();
-        var avatar = $('#avatar').val();
-        if (link==null || link=="",content==null || content=="")
-        {
-            alert("Please Fill Out All The Fields");
-            return false;
-        }
-        $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
 
-    });
-});
